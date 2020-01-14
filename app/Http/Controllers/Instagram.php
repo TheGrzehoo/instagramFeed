718a34ae49aced8daafaf23b0e3816676412b964
Grzegorz Kazimierski <gkazimierski@iai-sa.com>
2020-01-14 16:20:20 +0100

    inicjalizacja projektu laravela

---
+++ app/Http/Controllers/Instagram.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Instagram extends Controller
+{
+    public function getMediaData()
+    {
+    }
+
+    public function getMediaIDs()
+    {
+    }
+
+    public function getLongLivedToken()
+    {
+    }
+
+    public function getShortLivedToken()
+    {
+    }
+}
